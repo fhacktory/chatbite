d4bede98575ae740b4bd2ae374e9896a16a4c2a6
Stepoune <stepan.adamson@gmail.com>
2016-10-02 01:59:32 +0200

    DefaultScene.js => correct

--- chatbiteApp/components/DefaultScene.js
+++ chatbiteApp/components/DefaultScene.js
@@ -1,16 +1,79 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableHighlight , TextInput} from 'react-native';
 
 export default class DefaultScene extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: null };
+
+    this.onPressJoin = this.onPressJoin.bind(this);
+    this.onPressCreate =this.onPressCreate.bind(this);
+  }
+
+
+onPressJoin(){
+  if(this.state.text){
+    this.props.updateName(this.state.text);
+    fetch(this.props.urlServer+'join/session', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          username: this.state.text
+        })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+        console.log('responseJson', responseJson);
+        this.props.navigator.push({id: 'join'})
+      })
+    .catch((error) => {
+        console.error(error);
+    });
+  }
+}
+
+onPressCreate(){
+  if(this.state.text){
+    this.props.updateName(this.state.text);
+    fetch(this.props.urlServer+'create/session', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: this.state.text
+      })
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log('responseJson', responseJson);
+        this.props.navigator.push({id: 'create'})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
+
 
   render() {
     return (
       <View>
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+          />
           <Text>Choisis entre joindre ou créer</Text>
-          <TouchableHighlight onPress={()=>{this.props.navigator.push({id: 'join'})}}>
+          <TouchableHighlight onPress={this.onPressJoin}>
             <Text>Joindre</Text>
           </TouchableHighlight>
-          <TouchableHighlight onPress={()=>{this.props.navigator.push({id: 'create'})}}>
+          <TouchableHighlight onPress={this.onPressCreate}>
             <Text>Créer</Text>
           </TouchableHighlight>
       </View>
