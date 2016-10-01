69ef47d89b99f05012dfefd30824eed0ab0115e0
Stepoune <stepan.adamson@gmail.com>
2016-10-01 23:15:32 +0200

    chatbite-server => add api

--- chatbite-server/src/api/index.js
+++ chatbite-server/src/api/index.js
@@ -5,6 +5,9 @@ import facets from './facets';
 
 let session;
 let users;
+let arrayUsers;
+let prey;
+let predators;
 
 
 export default ({ config, db }) => {
@@ -22,13 +25,37 @@ export default ({ config, db }) => {
 		session = true;
 		users = {};
 		console.log('req', req.body.username);
-		user[req.body.username]=true;
+		users[req.body.username]={status:null, gps:{lat: null, long: null}};
 		res.json({ satus:true});
 	});
 
 	api.post('/join/session', (req, res) => {
-		user[req.body.username]=true;
+		users[req.body.username]={status:null, gps:{lat: null, long: null}};
 		res.json({ satus:true});
+	});
+
+	api.post('/start/session', (req, res) => {
+		arrayUsers = Object.keys(users);
+		prey = arrayUsers[Math.floor(Math.random() * arrayUsers.length)];
+		predators = {};
+		for (let i = 0; i < arrayUsers.length ; i ++ ) {
+			if(arrayUsers[i] !== prey){
+				users[arrayUsers[i]].status='predator';
+				predators[arrayUsers[i]] = {gps:{lat: null, long: null}};
+			}else{
+				users[arrayUsers[i]].status='prey';
+			}
+		}
+		users[prey].status = 'prey';
+		res.json({ satus:true, users : users, predators:predators, prey:users[prey]}); 
+	});
+
+	api.post('/get/position/predators', (req, res) => {
+		res.json({ satus:true, users : users, predators:predators, prey:users[prey]}); 
+	});
+
+	api.post('/update/position/user', (req, res) => {
+		users[req.body.username].gps = req.body.gps;
 	});
 
 	return api;
