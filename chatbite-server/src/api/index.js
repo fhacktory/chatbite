739afd0184c168da89adafa374583b1541d49f3e
Stepoune <stepan.adamson@gmail.com>
2016-10-01 19:45:35 +0200

    chatbite-server => api/index.js => add join and create session

--- chatbite-server/src/api/index.js
+++ chatbite-server/src/api/index.js
@@ -2,6 +2,11 @@ import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
 
+
+let session;
+let users;
+
+
 export default ({ config, db }) => {
 	let api = Router();
 
@@ -11,6 +16,19 @@ export default ({ config, db }) => {
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
+	});
+
+	api.post('/create/session', (req, res) => {
+		session = true;
+		users = {};
+		console.log('req', req.body.username);
+		user[req.body.username]=true;
+		res.json({ satus:true});
+	});
+
+	api.post('/join/session', (req, res) => {
+		user[req.body.username]=true;
+		res.json({ satus:true});
 	});
 
 	return api;
